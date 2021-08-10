3c1eb253986118d45e7e0fd496a87f017c30f935
Diego Ferreira <katesclau@gmail.com>
2021-08-10 16:24:06 -0300

    RDS example

---
+++ examples/relational-database/seed.sql
@@ -0,0 +1,8 @@
+CREATE TABLE posts (
+  id INT PRIMARY KEY NOT NULL,
+  user_id INT NOT NULL
+  title TEXT NOT NULL,
+  body TEXT NOT NULL
+);
+
+INSERT INTO posts (id, user_id, title, body) VALUES (1,1,'Title','A body paragraph');
