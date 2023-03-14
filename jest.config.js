df5882c60918f427358b741844d252bcf3a3304f
Hiroki Horiuchi <horiuchi.g@gmail.com>
2023-03-14 11:21:14 +0900

    fix: test error

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ const esmPackages = ['globby', 'slash'];
 /** @type {import('jest').Config} */
 module.exports = {
   transformIgnorePatterns: [`node_modules/(?!(${esmPackages.join('|')})/)`],
-  verbose: true,
+  snapshotFormat: {
+    escapeString: true,
+    printBasicPrototype: true,
+  },
 };
