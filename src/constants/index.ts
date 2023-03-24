a7987a8d3b892093c798bd10c09841665e66f137
Hiroki Horiuchi <horiuchi.g@gmail.com>
2023-03-24 11:45:10 +0900

    fix: remove unused constatns

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,14 +1,4 @@
-export const DEFAULT_MAPPING_TEMPLATE_LOCATION = 'mapping-templates';
 export const DEFAULT_ENCODING = 'utf8';
 export const DEFAULT_SCHEMA_FILE = 'schema.graphql';
-export const DEFAULT_HTTP_METHOD = 'POST';
-export const DEFAULT_RESOLVER_TYPE = 'UNIT';
 
 export type HTTPMessage = 'request' | 'response';
-export type MappingTemplateType = 'mappingTemplate' | 'functionConfiguration';
-export type SourceType =
-  | 'AMAZON_DYNAMODB'
-  | 'RELATIONAL_DATABASE'
-  | 'AWS_LAMBDA'
-  | 'AMAZON_ELASTICSEARCH'
-  | 'HTTP';
